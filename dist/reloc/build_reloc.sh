a397effa0c9ee21823b540c81d42471fc14eb2be
Takuya ASADA <syuu@scylladb.com>
2018-09-03 00:47:31 +0900

    build: add script to build relocatable package
    To build relocatable package easier, add build_reloc.sh to build it in
    one command.

---
+++ dist/reloc/build_reloc.sh
@@ -0,0 +1,74 @@
+#!/bin/bash -e
+
+. /etc/os-release
+
+print_usage() {
+    echo "build_reloc.sh --jobs 2"
+    echo "  --jobs  specify number of jobs"
+    echo "  --clean clean build directory"
+    echo "  --compiler  C++ compiler path"
+    echo "  --ninja     ninja path"
+    exit 1
+}
+
+JOBS=
+CLEAN=
+COMPILER=
+while [ $# -gt 0 ]; do
+    case "$1" in
+        "--jobs")
+            JOBS="-j$2"
+            shift 2
+            ;;
+        "--clean")
+            CLEAN=yes
+            shift 1
+            ;;
+        "--compiler")
+            COMPILER=$2
+            shift 2
+            ;;
+        *)
+            print_usage
+            ;;
+    esac
+done
+
+is_redhat_variant() {
+    [ -f /etc/redhat-release ]
+}
+is_debian_variant() {
+    [ -f /etc/debian_version ]
+}
+
+
+if [ ! -e dist/reloc/build_reloc.sh ]; then
+    echo "run build_reloc.sh in top of scylla dir"
+    exit 1
+fi
+
+NINJA=$(which ninja-build)
+if [ -z "$NINJA" ]; then
+    NINJA=$(which ninja)
+fi
+if [ -z "$NINJA" ]; then
+    echo "ninja not found."
+    exit 1
+fi
+
+if [ "$CLEAN" = "yes" ]; then
+    rm -rf build
+fi
+
+if [ -f build/release/scylla-package.tar.gz ]; then
+    rm build/release/scylla-package.tar.gz
+fi
+
+sudo ./install-dependencies.sh
+
+FLAGS="--with=scylla --with=iotune --mode=release"
+if [ -n "$COMPILER" ]; then
+    FLAGS="$FLAGS --compiler $COMPILER"
+fi
+./configure.py $FLAGS
+$NINJA $JOBS build/release/scylla-package.tar.gz
