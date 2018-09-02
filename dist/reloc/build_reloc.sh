7d6714827c401ea29e3f9cf7289e1f64123b727b
Takuya ASADA <syuu@scylladb.com>
2018-09-03 00:47:31 +0900

    build: add script to build relocatable package
    To build relocatable package easier, add build_reloc.sh to build it in
    one command.

---
+++ dist/reloc/build_reloc.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -e
+
+. /etc/os-release
+
+print_usage() {
+    echo "build_reloc.sh --jobs 2"
+    echo "  --jobs  specify number of jobs"
+    echo "  --clean clean build directory"
+    exit 1
+}
+
+JOBS=
+CLEAN=
+while [ $# -gt 0 ]; do
+    case "$1" in
+        "--jobs")
+            JOBS="-j$2"
+            shift 2
+            ;;
+        "--clean")
+            CLEAN=yes
+            shift 1
+            ;;
+        *)
+            print_usage
+            ;;
+    esac
+done
+
+
+if [ ! -e dist/reloc/build_reloc.sh ]; then
+    echo "run build_reloc.sh in top of scylla dir"
+    exit 1
+fi
+if [ "$ID" != "fedora" ]; then
+    echo "Only Fedora is supported distribution for build_reloc.sh"
+    exit 1
+fi
+
+if [ "$CLEAN" = "yes" ]; then
+    rm -rf build
+fi
+if [ -f build/release/scylla-package.tar ]; then
+    rm build/release/scylla-package.tar
+fi
+
+sudo ./install-dependencies.sh
+./SCYLLA-VERSION-GEN
+ninja-build $JOBS build/release/scylla-package.tar
